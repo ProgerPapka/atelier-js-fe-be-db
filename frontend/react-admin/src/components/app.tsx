d9b23dcf8462feee76efb13e410231f3d103f52b
Eldar Ziiatdinov <eldar_ziiatdinov@epam.com>
2019-06-25 23:33:39 +0400

    added material-ui and simple test app

--- frontend/react-admin/src/components/app.tsx
+++ frontend/react-admin/src/components/app.tsx
@@ -1,16 +1,53 @@
 import React from 'react';
 import '../styles/index.css';
+import Button from '@material-ui/core/Button';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import DialogTitle from '@material-ui/core/DialogTitle';
 
-export interface T {
-    a?: number;
-}
+export class App extends React.PureComponent {
 
-export class App extends React.PureComponent<T> {
+    state = {
+        isOpen: false
+    };
+
+    handleClickOpen = () => {
+        this.setState({isOpen: true});
+    }
+
+    handleClose = () => {
+        this.setState({isOpen: false});
+    }
+
     render() {
+        const {isOpen} = this.state;
         return (
             <div>
-                <h1>Hello World!</h1>
-                <h1>{this.props.a}</h1>
+                <Button variant="contained" color="secondary" onClick={this.handleClickOpen}>Click me</Button>
+                <Dialog
+                    open={isOpen}
+                    onClose={this.handleClose}
+                    aria-labelledby="alert-dialog-title"
+                    aria-describedby="alert-dialog-description"
+                >
+                    <DialogTitle id="alert-dialog-title">{"Use Google's location service?"}</DialogTitle>
+                    <DialogContent>
+                    <DialogContentText id="alert-dialog-description">
+                        Let Google help apps determine location. This means sending anonymous location data to
+                        Google, even when no apps are running.
+                    </DialogContentText>
+                    </DialogContent>
+                    <DialogActions>
+                    <Button onClick={this.handleClose} color="primary">
+                        Disagree
+                    </Button>
+                    <Button onClick={this.handleClose} color="primary" autoFocus>
+                        Agree
+                    </Button>
+                    </DialogActions>
+                </Dialog>
             </div>
         );
     }
--- frontend/react-admin/src/index.tsx
+++ frontend/react-admin/src/index.tsx
@@ -2,4 +2,8 @@ import ReactDOM from 'react-dom';
 import React from 'react';
 import {App} from './components/app';
 
-ReactDOM.render(<App a={12}/>, document.getElementById('root'));
+const renderApp = () => {
+    ReactDOM.render(<App />, document.getElementById('root'));
+};
+
+renderApp();
