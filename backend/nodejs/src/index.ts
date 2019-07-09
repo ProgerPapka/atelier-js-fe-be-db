fabc43bd8efe737be58b816e83badf39e10461b2
Eldar Ziiatdinov <eldar_ziiatdinov@epam.com>
2019-07-09 18:37:45 +0400

    added rules tslint and simple graphql schema

--- backend/nodejs/src/index.ts
+++ backend/nodejs/src/index.ts
@@ -1,15 +1,26 @@
 import express from 'express';
+import graphqlHTTP from 'express-graphql';
+import { buildSchema } from 'graphql';
+const port = 3000;
+
+const schema = buildSchema(`
+  type Query {
+    postTitle: String,
+    blogTitle: String
+  }
+`);
+
+const root = {
+    blogTitle: () => ('scotch.io'),
+    postTitle: () => ('Build a Simple GraphQL Server With Express and NodeJS')
+};
+
 const app = express();
-const port = 8080;
+app.use('/', graphqlHTTP({
+    graphiql: true,
+    rootValue: root,
+    schema
+}));
 
-app.get( '/', ( req, res ) => {
-    res.send( 'Hello world!' );
-} );
-
-app.get( '/test', ( req, res ) => {
-    res.send( 'Test http-get' );
-} );
-
-app.listen( port, () => {
-    console.log( `server started at http://localhost:${ port }` );
-} );
+app.listen(port);
+console.log('GraphQL API server running at localhost: ' + port);
